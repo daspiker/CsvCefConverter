0ad0212db3be06dc783d35bfa79ca2fec4e82b1e
dennis pike <daspiker@gmail.com>
2021-06-04 09:20:54 -0400

    added arguments and logic around using them

--- CsvCefConverter.py
+++ CsvCefConverter.py
@@ -3,14 +3,25 @@
 ## started with work from Frank Cardinale at https://www.frankcardinale.com/2020/04/27/script-to-read-from-csv-file-and-write-to-syslog-in-cef-format/
 
 ## Importing the libraries used in the script
+import sys
 import syslog
 import csv
 
+##check to see number of arguments and if less than 1 then print help
+if len(sys.argv) < 2:
+    print("This script requires the first argument.")
+    print("python CsvCefConverter.py <template csv filename to read in> <cef filename to write to>")
+    print("if cef filename is not provided then CefOutputFile.txt is used")
+    exit()
+
 ##open csv file
-with open('./testFile.csv') as csvfile:
+with open(sys.argv[1]) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     #loop through each row of csv
-    outputFile = open('./CEFLogFile.txt', 'w')
+    if len(sys.argv) > 2:
+        outputFile = open(sys.argv[2], 'w')
+    else:
+        outputFile = open('./CefOutputFile.txt', 'w')
 
     for row in readCSV:
 
